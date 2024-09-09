c60fa1bd6f199188ff502bacf99f9808ff67c5c4
Gabriel Valcacer Torquato <gabrielvalcacertorquato@gmail.com>
2024-09-09 17:15:05 -0300

    Create 04.trigger.sql

---
+++ 04.trigger.sql
@@ -0,0 +1,19 @@
+DELIMITER //
+CREATE TRIGGER AtualizarCustoTratamento
+AFTER INSERT ON Tratamento_Procedimento
+FOR EACH ROW
+BEGIN
+     DECLARE custo_total DECIMAL(10, 2);
+     
+     -- Calcula o custo total do tratamento
+     SELECT SUM(tp-quantidade * p.custo) INTO custo_total
+     FROM Tratamento_Procedimento tp
+     JOIN Procedimento p ON tp.procedimento_id = p.procedimento_id
+     WHERE tp.tratamento_id = NEW.tratamento_id;
+     
+     -- Atualiza o valor total do tratamento na tabela Tratamento
+     UPDATE Tratamento
+     SET valor_total = custo_total
+     WHERE tratamento_id = NEW.tratamento_id;
+END//
+DELIMITER ;
